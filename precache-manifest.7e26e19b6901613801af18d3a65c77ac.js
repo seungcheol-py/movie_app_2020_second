490b9af64a64ae49dcb2446d53d77c320f0c188c
Seungcheol Oh <seung9412@naver.com>
2020-07-28 04:53:09 +0900

    Updates

---
+++ precache-manifest.7e26e19b6901613801af18d3a65c77ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f5fdc79bacbb7f24e8d1a2823d3104",
+    "url": "/movie_app_2020_second/index.html"
+  },
+  {
+    "revision": "7fe453a9b93208785a3c",
+    "url": "/movie_app_2020_second/static/css/main.eedfbc92.chunk.css"
+  },
+  {
+    "revision": "3051787edaf9869e76a0",
+    "url": "/movie_app_2020_second/static/js/2.5efdebbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020_second/static/js/2.5efdebbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe453a9b93208785a3c",
+    "url": "/movie_app_2020_second/static/js/main.984e255d.chunk.js"
+  },
+  {
+    "revision": "7e7f3304364196e7f5a8",
+    "url": "/movie_app_2020_second/static/js/runtime-main.97b9f06a.js"
+  }
+]);
